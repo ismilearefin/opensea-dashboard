ba1fdf6d5a25e34fdbdb99a1fd306099825aa332
ismilearefin <arefinismile@gmail.com>
2023-10-25 01:31:09 +0600

    Filter button section design

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,18 @@
 import Navbar from '@/components/navbar/Navbar'
 import styles from './page.module.css'
+import { MdOutlineFilterList } from 'react-icons/md'
+
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Navbar></Navbar>
+      {/* filter-toggle Button */}
+      <div className={styles.buttonDivFilter}>
+        <button className={styles.filterToggle}>
+          <MdOutlineFilterList></MdOutlineFilterList>
+        </button>
+      </div>
     </main>
   )
 }
