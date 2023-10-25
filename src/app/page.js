f05a7454602ee8298ae87dfc633d638c64df8090
ismilearefin <arefinismile@gmail.com>
2023-10-25 18:29:16 +0600

    table hover effect & aside section (filter options)

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,20 @@
 "use client";
 import Navbar from "@/components/navbar/Navbar";
 import styles from "./page.module.css";
-import { MdOutlineFilterList } from "react-icons/md";
+import { MdOutlineFilterList,MdExpandLess,MdExpandMore } from "react-icons/md";
 import CustomButton from "@/components/button/CustomButton";
 import { useState, useEffect } from "react";
 import TableForDashboard from "@/components/tableForDashboard/TableForDashboard";
 
 export default function Home() {
-  const storedToggle =  localStorage.getItem("toggle");
+  const storedToggle = localStorage.getItem("toggle");
   const [toggle, setToggle] = useState(
     storedToggle ? JSON.parse(storedToggle) : false
-    );
+  );
 
   useEffect(() => {
     localStorage.setItem("toggle", JSON.stringify(toggle));
   }, [toggle]);
-  
 
   return (
     <main className={styles.main}>
@@ -31,16 +30,15 @@ export default function Home() {
       <div className={styles.tableSection}>
         {/* filter-toggle Section */}
         {toggle && (
-          <aside>
-            <button>Event Type</button>
-            <button>Collections</button>
-            <button>Chains</button>
+          <aside className={styles.filterOptions}>
+            <button>Event Type <MdExpandMore className={styles.icons} /></button>
+            <button>Collections <MdExpandMore className={styles.icons}/></button>
+            <button>Chains <MdExpandMore className={styles.icons}/></button>
           </aside>
         )}
         {/* Table Section Design */}
-        
-            <TableForDashboard></TableForDashboard>
-        
+
+        <TableForDashboard></TableForDashboard>
       </div>
     </main>
   );
